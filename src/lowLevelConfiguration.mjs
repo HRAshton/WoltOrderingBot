ed88fa4e80cdac332ca1bab7cf897931921d84e7
HRAshton <12210721+hrashton@users.noreply.github.com>
2023-11-20 23:22:53 +0400

    Convert environment values to int

--- src/lowLevelConfiguration.mjs
+++ src/lowLevelConfiguration.mjs
@@ -1,6 +1,6 @@
-export const ORDERS_CLEANUP_INTERVAL_SECS = process.env.ORDERS_CLEANUP_INTERVAL_SECS || 60 * 15;
+export const ORDERS_CLEANUP_INTERVAL_SECS = parseInt(process.env.ORDERS_CLEANUP_INTERVAL_SECS || 60 * 15);
 
-export const REFRESH_TOKENS_INTERVAL_SECS = process.env.REFRESH_TOKENS_INTERVAL_SECS || 60 * 30;
+export const REFRESH_TOKENS_INTERVAL_SECS = parseInt(process.env.REFRESH_TOKENS_INTERVAL_SECS || 60 * 30);
 
 export const SQLITE_DATABASE_PATH = process.env.SQLITE_DATABASE_PATH || 'database.db';
 
